1be05d5eb043d517e1b221c9b6549105a95bb4c6
ieheath13 <ieheath13@gmail.com>
2016-04-24 13:27:53 -0700

    fixed

--- plot2.R
+++ plot2.R
@@ -6,12 +6,13 @@ electric_consumption$DateTime <- paste(electric_consumption$Date, electric_consu
 used_data <- electric_consumption[electric_consumption$Date >= "2007-02-01" & electric_consumption$Date <= "2007-02-03",]
 names(used_data)
 str(used_data)
-<<<<<<< HEAD
+
+#Plot and save plot 2
 png(filename = "/Users/ianeheath/Desktop/Exploratory_data_Assignment_1/plot2.png")
 plot(used_data$DateTime, used_data$Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
 lines(used_data$DateTime, used_data$Global_active_power)
 dev.off()
-=======
-plot(used_data$DateTime, used_data$Global_active_power)
+
+
      
->>>>>>> f3ed92ccdbd4f95b1da3e8c964bf89e1f1914598
+
