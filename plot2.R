771da6b77809918326065ccf78ce8441b2d3f6fa
ieheath13 <ieheath13@gmail.com>
2016-04-24 13:18:49 -0700

    Plot 2 done

---
+++ plot2.R
@@ -0,0 +1,12 @@
+electric_consumption <- read.table("/Users/ianeheath/Desktop/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+electric_consumption$Date <- as.Date(electric_consumption$Date, format = "%d/%m/%Y")
+
+electric_consumption$DateTime <- strptime(electric_consumption$DateTime, format = "%d/%m/%Y %H:%M:%S")
+electric_consumption$DateTime <- paste(electric_consumption$Date, electric_consumption$Time, sep = " ")
+used_data <- electric_consumption[electric_consumption$Date >= "2007-02-01" & electric_consumption$Date <= "2007-02-03",]
+names(used_data)
+str(used_data)
+png(filename = "/Users/ianeheath/Desktop/Exploratory_Analysis_Assignment_1/plot2.png")
+plot(used_data$DateTime, used_data$Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
+lines(used_data$DateTime, used_data$Global_active_power)
+dev.off()
